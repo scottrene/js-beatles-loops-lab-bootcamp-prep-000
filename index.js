4f8bdc23ffd657295bc272ae6369d4155f7e22ae
michele lafleur <mlafleurr@gmail.com>
2018-08-13 01:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+var i=0;
+
+function theBeatlesPlay (musicians, instruments) {
+  var beatles = [];
+  for (i=0; i < musicians.length; i++) {
+    beatles.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatles;
+}
+
+function johnLennonFacts (facts) {
+  var i=facts.length - 1;
+  while (i >= 0) {
+    facts[i] = facts[i] + "!!!";
+    i--;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles (number) {
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+  } while (number < 15) {
+    number = number++;
+  }
+  return i;
+}
